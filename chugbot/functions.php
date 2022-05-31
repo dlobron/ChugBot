68bdb89bab0f6a1f8891d913f21480eb8904ffa9
Brian Roth <broth@embarkvet.com>
2022-05-31 00:24:44 -0400

    Update PHPMailer

--- chugbot/functions.php
+++ chugbot/functions.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'constants.php';
 include_once 'functions.php';
-require_once 'PHPMailer/PHPMailerAutoload.php';
+require_once 'PHPMailer/PHPMailer.php';
 
 function getArchiveYears(&$dbErr)
 {
