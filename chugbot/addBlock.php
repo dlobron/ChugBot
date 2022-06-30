652ace5ddaae28cc5f56589a6f7d88659c4e95aa
David Lobron <dlobron@sea-machines.com>
2022-06-30 17:07:09 -0400

    Add visible checkbox for add block, defaulting to true.

--- chugbot/addBlock.php
+++ chugbot/addBlock.php
@@ -8,6 +8,7 @@ $addBlockPage = new AddPage("Add Block",
     "Please enter information for this block",
     "blocks", "block_id");
 $addBlockPage->addColumn("name");
+$addBlockPage->addColumn("visible_to_campers", true, true);
 $addBlockPage->addInstanceTable("block_instances");
 $secondParagraph = <<<EOM
 A block is a time period for an activity: for example, weeks 1-2 of July.  Each block is associated with one or more sessions: for example, "July 1" might be associated with July and July+August sessions (a session is the unit of time that a camper signs up for).  You can add or edit sessions for this block later if you are not sure right now which sessions to assign.
@@ -25,13 +26,18 @@ $nameField->setError($addBlockPage->errForColName("name"));
 $nameField->setGuideText("Choose a name for this block (e.g., \"July Week 1\", \"Mini Session Aleph\", etc.)");
 $addBlockPage->addFormItem($nameField);
 
-$sessionChooserField = new FormItemInstanceChooser("Sessions", false, "session_ids", 1);
+$visibleToCampersChoiceBox = new FormItemCheckBox("Visible to Campers", false, "visible_to_campers", 1);
+$visibleToCampersChoiceBox->setGuideText("Check this box to make this block visible and active when campers choose their next round of chugim.");
+$visibleToCampersChoiceBox->setInputValue(true);
+$addBlockPage->addFormItem($visibleToCampersChoiceBox);
+
+$sessionChooserField = new FormItemInstanceChooser("Sessions", false, "session_ids", 2);
 $sessionChooserField->setId2Name($addBlockPage->instanceId2Name);
 $sessionChooserField->setActiveIdHash($addBlockPage->instanceActiveIdHash);
 $sessionChooserField->setGuideText("Choose each session that contains this time block (you can do this later if you are not sure now).");
 $addBlockPage->addFormItem($sessionChooserField);
 
-$edahChooser = new FormItemInstanceChooser("Edot", false, "edot_for_block", 2);
+$edahChooser = new FormItemInstanceChooser("Edot", false, "edot_for_block", 3);
 $edahChooser->setId2Name($addBlockPage->activeEdotFilterId2Name);
 $edahChooser->setActiveIdHash($addBlockPage->activeEdotHash);
 $edahChooser->setGuideText("Choose the edot who will participate in this time block (you can do this later if you are not sure now)");
